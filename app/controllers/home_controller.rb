e19877e97f884348590204f7f109ece0a58d3cb8
Aaron Groch <ar.groch@gmail.com>
2016-05-18 09:07:43 -0400

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,18 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def results
+  	@food = params[:food]
+  	@location = params[:location]
+  	parameters = { term: @food, limit: 9 }
+
+  	@results = Yelp.client.search(@location, parameters)
+  end
+
+  def test
+    parameters = { term: "pancakes", limit: 10 }
+
+    @results = Yelp.client.search('Atlanta, GA', parameters)
+  end
+end
